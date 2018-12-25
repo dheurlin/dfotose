7d1b8e1c3f1c392386b69281058f5a6b222d2120
Daniel Heurlin <daniel.heurlin@gmail.com>
2018-12-25 18:48:40 +0100

    redo image api in terms of generic gallery-entry api

---
+++ src/server/routes/gallery-entry-api.js
@@ -0,0 +1,216 @@
+import _ from 'lodash';
+import {Router} from 'express';
+import bodyParser from 'body-parser';
+import moment from 'moment';
+
+import Gallery from '../model/gallery';
+import User from '../model/user';
+import ImageTag from '../model/image-tag';
+import {requireRestrictions, hasRestrictions} from './auth-api';
+import {Restrictions} from '../model/user-roles';
+
+import Logger from '../logger';
+import {abortOnError} from '../utils';
+
+
+// Returns a router prepopulated with the routes common to gallery entries,
+// given the database model class to be used to write and read data
+const galleryEntryRouter = className => {
+
+  const jsonParser = bodyParser.json();
+  const router = Router();
+
+  // Return all entries for a specific gallery
+  router.get('/:galleryId', (req, res) => {
+    const galleryId = req.params.galleryId;
+
+    className.find({galleryId: galleryId}).sort('shotAt').exec((err, entries) => {
+      if (err) {
+        res.status(500).send(err);
+        throw err;
+      }
+
+      res.send(entries);
+    });
+  });
+
+  router.get('/:id/details', (req,res) => {
+    const id = req.params.id;
+    className.findById(id, (err, entry) => {
+      abortOnError(err, res);
+      res.send(entry);
+    });
+  });
+
+
+  router.get('/:id/tags', (req, res) => {
+    const id = req.params.id;
+    ImageTag.find({ imageId: id }, (err, imageTags) => {
+      abortOnError(err, res);
+
+      res.send(imageTags);
+    });
+  });
+
+  router.get('/:id/author', (req, res) => {
+      const id = req.params.id;
+      className.findById(id, (err, entry) => {
+          abortOnError(err, res);
+          res.send(entry.author);
+      });
+  })
+
+  router.post('/:id/gallerythumbnail', (req,res) => {
+    const id = req.params.id;
+
+    const canWriteImage = hasRestrictions(
+        req,
+        Restrictions.WRITE_GALLERY | Restrictions.WRITE_IMAGES
+    );
+
+    if (!canWriteImage) {
+      res.status(403).end();
+      Logger.warn(`User ${req.session.user.cid} had insufficient permissions to change thumbnail.`);
+      return;
+    }
+
+    // Find the entry that should be set as thumbnail
+    className.findOne({_id: id}, (err, newThumb) => {
+      abortOnError(err, res);
+
+      // Remove the entry that was previously thumbnail
+      className.find({galleryId: newThumb.galleryId, isGalleryThumbnail: true}, (err, oldThumbs) => {
+        if (oldThumbs !== null && oldThumbs.length !== 0) {
+          oldThumbs.forEach(oldThumb => {
+            oldThumb.isGalleryThumbnail = false;
+            oldThumb.save();
+          });
+        }
+        else { console.log("No old thumbnail found"); }
+      });
+
+      // Set the new one as thumbnail
+      newThumb.isGalleryThumbnail = true;
+      newThumb.save();
+      console.log(`Changed gallery thumbnail to ${id} for gallery ${newThumb.galleryId}`);
+      res.status(202).end();
+
+    });
+  })
+
+  router.post('/:id/author', jsonParser, (req, res) => {
+      const entryId = req.params.id;
+      const {newCid} = req.body;
+
+      const canWriteImage = hasRestrictions(
+          req,
+          Restrictions.WRITE_GALLERY | Restrictions.WRITE_IMAGES
+      );
+
+      if (!canWriteImage) {
+        res.status(403).end();
+        Logger.warn(`User ${req.session.user.cid} had insufficient permissions to change author`);
+        return;
+      }
+
+      // First find the user so we can get the fullname
+      User.findOne({cid: newCid}, (err, user) => {
+          abortOnError(err, res);
+
+          // Next, update the entry
+          className.findOneAndUpdate({_id: entryId}, {
+            $set: {
+              authorCid: newCid,
+              author: user.fullname
+            }
+          }, (err) => {
+            abortOnError(err, res);
+
+            console.log(`Changed author to ${user.fullname} for gallery entry ${entryId}`);
+            res.status(202).end();
+          })
+      });
+  });
+
+  router.post('/:id/tags', jsonParser, (req, res) => {
+    const imageId = req.params.id;
+
+    const {tagName} = req.body;
+    const filteredTagName = inHTMLData(tagName).toLowerCase();
+
+    const imageTagData = {
+      imageId: imageId,
+      tagName: filteredTagName
+    };
+
+    var newTag = new classNameTag(imageTagData);
+    newTag.save((err) => {
+      abortOnError(err, res);
+
+      // Now add a duplicate to the images list of tags
+      className.findById(imageId, (err, image) => {
+        abortOnError(err, res);
+
+        image.tags.push(filteredTagName);
+        const newImageTags = image.tags;
+
+        className.findOneAndUpdate({ _id: imageId }, {
+          $set: {
+            tags: newImageTags
+          }
+        }, (err) => {
+          abortOnError(err, res);
+
+          console.log(`Added tag ${filteredTagName} to image ${imageId}`);
+          res.status(202).end();
+        });
+      });
+    });
+  });
+
+  router.get('/tags/:tagName/search', (req, res) => {
+    const tagName = req.params.tagName.toLowerCase();
+
+    ImageTag.find({ tagName: tagName }, (err, imageTags) => {
+      abortOnError(err, res);
+
+      // imageTags contains all of the ids of images we need to send
+      // to the client
+      const imageObjectIds = _.map(imageTags, tag => {
+        return mongoose.Types.ObjectId(tag.imageId);
+      });
+
+      className.find({ '_id': {
+        $in: imageObjectIds
+      }}, (err, images) => {
+        abortOnError(err, res);
+
+        res.send(images);
+      });
+    });
+  });
+
+
+  router.delete('/:id',
+    requireRestrictions(Restrictions.WRITE_IMAGES | Restrictions.WRITE_GALLERY),
+    (req, res) => {
+    const id = req.params.id;
+
+    className.findByIdAndRemove(id, (err, image) => {
+      if (err) {
+        res.status(500).send(err);
+        throw err;
+      }
+
+      Logger.info(`User ${req.session.user.cid} removed image ${id}`);
+
+      res.status(202).send();
+    });
+  });
+
+  return router;
+
+};
+
+
+export default galleryEntryRouter;
--- src/server/routes/image-api.js
+++ src/server/routes/image-api.js
@@ -20,11 +20,12 @@ import {abortOnError} from '../utils';
 const jsonParser = bodyParser.json();
 
 import Image from '../model/image';
-import ImageTag from '../model/image-tag';
 import Gallery from '../model/gallery';
-import User from '../model/user';
 
-const router = Router();
+import galleryEntryRouter from './gallery-entry-api.js'
+
+const router = galleryEntryRouter(Image);
+
 export default router;
 
 const imageStorage = multer.diskStorage({
@@ -52,28 +53,6 @@ fs.mkdirs(config.storage.path, (err) => {
     Logger.error(`Could not create storage directory ${config.storage.path}`);
     throw err;
   }
-});
-
-// Return all images for a specific gallery
-router.get('/:galleryId', (req, res) => {
-  const galleryId = req.params.galleryId;
-
-  Image.find({galleryId: galleryId}).sort('shotAt').exec((err, images) => {
-    if (err) {
-      res.status(500).send(err);
-      throw err;
-    }
-
-    res.send(images);
-  });
-});
-
-router.get('/:id/details', (req,res) => {
-  const id = req.params.id;
-  Image.findById(id, (err, image) => {
-    abortOnError(err, res);
-    res.send(image);
-  });
 });
 
 // Return a specific image using an id
@@ -113,153 +92,6 @@ router.get('/:id/preview', (req, res) => {
     }
 
     res.sendFile(image.preview);
-  });
-});
-
-router.get('/:id/tags', (req, res) => {
-  const id = req.params.id;
-  ImageTag.find({ imageId: id }, (err, imageTags) => {
-    abortOnError(err, res);
-
-    res.send(imageTags);
-  });
-});
-
-router.get('/:id/author', (req, res) => {
-    const id = req.params.id;
-    Image.findById(id, (err, image) => {
-        abortOnError(err, res);
-        res.send(image.author);
-    });
-})
-
-router.post('/:id/gallerythumbnail', (req,res) => {
-  const id = req.params.id;
-
-  const canWriteImage = hasRestrictions(
-      req,
-      Restrictions.WRITE_GALLERY | Restrictions.WRITE_IMAGES
-  );
-
-  if (!canWriteImage) {
-    res.status(403).end();
-    Logger.warn(`User ${req.session.user.cid} had insufficient permissions to change thumbnail.`);
-    return;
-  }
-
-  // Find the image that should be set as thumbnail
-  Image.findOne({_id: id}, (err, newThumb) => {
-    abortOnError(err, res);
-
-    // Remove the image that was previously thumbnail
-    Image.find({galleryId: newThumb.galleryId, isGalleryThumbnail: true}, (err, oldThumbs) => {
-      if (oldThumbs !== null && oldThumbs.length !== 0) {
-        oldThumbs.forEach(oldThumb => {
-          oldThumb.isGalleryThumbnail = false;
-          oldThumb.save();
-        });
-      }
-      else { console.log("No old thumbnail found"); }
-    });
-
-    // Set the new one as thumbnail
-    newThumb.isGalleryThumbnail = true;
-    newThumb.save();
-    console.log(`Changed gallery thumbnail to ${id} for gallery ${newThumb.galleryId}`);
-    res.status(202).end();
-
-  });
-})
-
-router.post('/:id/author', jsonParser, (req, res) => {
-    const imageId = req.params.id;
-    const {newCid} = req.body;
-
-    const canWriteImage = hasRestrictions(
-        req,
-        Restrictions.WRITE_GALLERY | Restrictions.WRITE_IMAGES
-    );
-
-    if (!canWriteImage) {
-      res.status(403).end();
-      Logger.warn(`User ${req.session.user.cid} had insufficient permissions to change author`);
-      return;
-    }
-
-    // First find the user so we can get the fullname
-    User.findOne({cid: newCid}, (err, user) => {
-        abortOnError(err, res);
-
-        // Next, update the image
-        Image.findOneAndUpdate({_id: imageId}, {
-          $set: {
-            authorCid: newCid,
-            author: user.fullname
-          }
-        }, (err) => {
-          abortOnError(err, res);
-
-          console.log(`Changed author to ${user.fullname} for image ${imageId}`);
-          res.status(202).end();
-        })
-    });
-});
-
-router.post('/:id/tags', jsonParser, (req, res) => {
-  const imageId = req.params.id;
-
-  const {tagName} = req.body;
-  const filteredTagName = inHTMLData(tagName).toLowerCase();
-
-  const imageTagData = {
-    imageId: imageId,
-    tagName: filteredTagName
-  };
-
-  var newTag = new ImageTag(imageTagData);
-  newTag.save((err) => {
-    abortOnError(err, res);
-
-    // Now add a duplicate to the images list of tags
-    Image.findById(imageId, (err, image) => {
-      abortOnError(err, res);
-
-      image.tags.push(filteredTagName);
-      const newImageTags = image.tags;
-
-      Image.findOneAndUpdate({ _id: imageId }, {
-        $set: {
-          tags: newImageTags
-        }
-      }, (err) => {
-        abortOnError(err, res);
-
-        console.log(`Added tag ${filteredTagName} to image ${imageId}`);
-        res.status(202).end();
-      });
-    });
-  });
-});
-
-router.get('/tags/:tagName/search', (req, res) => {
-  const tagName = req.params.tagName.toLowerCase();
-
-  ImageTag.find({ tagName: tagName }, (err, imageTags) => {
-    abortOnError(err, res);
-
-    // imageTags contains all of the ids of images we need to send
-    // to the client
-    const imageObjectIds = _.map(imageTags, tag => {
-      return mongoose.Types.ObjectId(tag.imageId);
-    });
-
-    Image.find({ '_id': {
-      $in: imageObjectIds
-    }}, (err, images) => {
-      abortOnError(err, res);
-
-      res.send(images);
-    });
   });
 });
 
@@ -434,22 +266,3 @@ export function updateAuthorOfImagesUploadedByCid(cid, author) {
   });
 }
 
-// Delete a specific image
-//  - Note: this automatically removes all gallery
-//          associations.
-router.delete('/:id',
-  requireRestrictions(Restrictions.WRITE_IMAGES | Restrictions.WRITE_GALLERY),
-  (req, res) => {
-  const id = req.params.id;
-
-  Image.findByIdAndRemove(id, (err, image) => {
-    if (err) {
-      res.status(500).send(err);
-      throw err;
-    }
-
-    Logger.info(`User ${req.session.user.cid} removed image ${id}`);
-
-    res.status(202).send();
-  });
-});
